e5cb13832ae5775974c9611466912d886ad4cc9f
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-05-20 22:36:43 +0300

    add header with nav-items and logo

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import headerStyles from './app-header.module.css';
+
+import AppHeaderItem from "./app-header-item/app-header-item";
+import { BurgerIcon, ListIcon, ProfileIcon, Logo } from '@ya.praktikum/react-developer-burger-ui-components'
+
+
+export default class AppHeader extends React.Component {
+    render() {
+        return (
+            <header className={ headerStyles.header }>
+                <nav className={ headerStyles.header__nav }>
+                    <ul className={ headerStyles.header__list }>
+                        <AppHeaderItem text="Конструктор">
+                            <BurgerIcon type="primary"/>
+                        </AppHeaderItem>
+                        <AppHeaderItem text="Лента заказов">
+                            <ListIcon type="primary" />
+                        </AppHeaderItem>
+                        <AppHeaderItem text="Личный кабинет">
+                            <ProfileIcon type="primary" />
+                        </AppHeaderItem>
+                    </ul>
+                </nav>
+                <div className={headerStyles.header__logo}>
+                    <Logo/>
+                </div>
+            </header>
+        )
+    }
+}
---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import AppHeader from "../app-header/app-header";
+
+function App() {
+  return (
+      <div className='root__section'>
+        <AppHeader/>
+      </div>
+  );
+}
+
+export default App;
