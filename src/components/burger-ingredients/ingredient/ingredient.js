43e2dcab54310e44bf56a1408b8f01624b2a47e4
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-05-23 17:24:25 +0300

    refactor ingredients

--- src/components/burger-ingredients/ingredient/ingredient.js
+++ src/components/burger-ingredients/ingredient/ingredient.js
@@ -1,39 +1,21 @@
 import React from "react";
 
+import ingredientStyles from './ingredient.module.css';
 import { Counter, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 export default class Ingredient extends React.Component {
     render() {
         return (
-            <li
-                className="dashed"
-                key={ this.props.item._id }
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    position: "relative"
-                }}
-            >
+            <li className={`${ ingredientStyles.ingredient } dashed`}>
                 <img className="dashed" src={ this.props.item.image } alt={ this.props.item.name }/>
-                <div className="text text_type_digits-default mt-1 mb-1 dashed" style={{
-                    display: "flex",
-                }}>
-                    <p style={{
-                        margin: 0,
-                        marginRight: 9,
-                    }}>{ this.props.item.price }  </p>
+                <div className={`${ ingredientStyles.ingredient__priceWrapper } text text_type_digits-default mt-1 mb-1 dashed`}>
+                    <p className={ ingredientStyles.ingredient__price }>{ this.props.item.price }</p>
                     <CurrencyIcon className="ml-2" type="primary dashed" />
                 </div>
-                <p className="text text_type_main-default dashed" style={{
-                    textAlign: "center",
-                    paddingBottom: 24,
-                }}>{ this.props.item.name }</p>
-                <Counter count={1} size="default" style={{
-                    position: 'relative',
-                    top: 0,
-                    bottom: 0,
-                }} />
+                <p
+                    className={`text text_type_main-default ${ ingredientStyles.ingredient__name } dashed`}
+                >{ this.props.item.name }</p>
+                <Counter count={1} size="default"/>
             </li>
         );
     }
--- src/components/burger-ingredients/ingredients/ingredients.js
+++ src/components/burger-ingredients/ingredients/ingredients.js
@@ -1,28 +1,18 @@
 import React from "react";
 
+import ingredientStyles from './ingredients.module.css';
 import Ingredient from "../ingredient/ingredient";
 
 export default class Ingredients extends React.Component {
     render() {
         return (
-            <div
-                className="mt-10 mb-6 pl-1 pr-1"
-                style={{
-                    height: '53vh',
-                    overflowY: 'scroll',
-                }}
-            >
+            <div className={`${ingredientStyles.ingredients} mt-10 mb-6 pl-1 pr-1`}>
                 <h4 className="text text_type_main-medium dashed mb-6">{ this.props.title }</h4>
-                <ul className="dashed" style={{
-                    listStyleType: 'none',
-                    padding: 0,
-                    display: 'grid',
-                    gridTemplateColumns: '1fr 1fr',
-                }}>
+                <ul className={`${ingredientStyles.ingredients__list} dashed`}>
                     {
                         this.props.data.map(item => {
                             if (item.type === this.props.activeMealType) {
-                                return <Ingredient item={ item } />;
+                                return <Ingredient key={ item._id } item={ item } />;
                             } else {
                                 return null;
                             }
