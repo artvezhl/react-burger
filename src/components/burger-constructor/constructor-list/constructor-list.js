49278bb4acd638eb475e68005a5502218ab6119e
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-05-26 22:52:43 +0300

    add drag&drop buttons

--- src/components/burger-constructor/constructor-list/constructor-list.js
+++ src/components/burger-constructor/constructor-list/constructor-list.js
@@ -1,56 +1,78 @@
 import React from "react";
 
-import ListStyles from './constructor-list.module.css';
-import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import listStyles from './constructor-list.module.css';
+import { ConstructorElement, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 export default class ConstructorList extends React.Component {
     render() {
         return (
-            <div className={ ListStyles.constructorList }>
-                <ConstructorElement
-                    type="top"
-                    isLocked={true}
-                    text="Краторная булка N-200i (верх)"
-                    price={200}
-                    thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={350}
-                    thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={350}
-                    thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={350}
-                    thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={350}
-                    thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={350}
-                    thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={350}
-                    thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
-                />
-                <ConstructorElement
-                    type="bottom"
-                    isLocked={true}
-                    text="Краторная булка N-200i (низ)"
-                    price={200}
-                    thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-                />
+            <div className={ listStyles.constructorList }>
+                <article>
+                    <ConstructorElement
+                        type="top"
+                        isLocked={true}
+                        text="Краторная булка N-200i (верх)"
+                        price={200}
+                        thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                    />
+                </article>
+                <article className={ listStyles.item }>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                        text="Говяжий метеорит (отбивная)"
+                        price={350}
+                        thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
+                    />
+                </article>
+                <article className={ listStyles.item }>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                        text="Говяжий метеорит (отбивная)"
+                        price={350}
+                        thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
+                    />
+                </article>
+                <article className={ listStyles.item }>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                        text="Говяжий метеорит (отбивная)"
+                        price={350}
+                        thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
+                    />
+                </article>
+                <article className={ listStyles.item }>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                        text="Говяжий метеорит (отбивная)"
+                        price={350}
+                        thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
+                    />
+                </article>
+                <article className={ listStyles.item }>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                        text="Говяжий метеорит (отбивная)"
+                        price={350}
+                        thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
+                    />
+                </article>
+                <article className={ listStyles.item }>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                        text="Говяжий метеорит (отбивная)"
+                        price={350}
+                        thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
+                    />
+                </article>
+                <article>
+                    <ConstructorElement
+                        type="bottom"
+                        isLocked={true}
+                        text="Краторная булка N-200i (низ)"
+                        price={200}
+                        thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                    />
+                </article>
             </div>
         )
     }
