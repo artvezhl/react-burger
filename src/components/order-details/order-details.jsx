96c19dbe6d04f6366503c4bade0accd92356dd8f
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-06-25 23:14:16 +0300

    add redux with toolkit and react-dnd

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext} from "react";
 
 import orderStyles from "./order-details.module.css";
 import doneButton from "../../images/done.svg";
