23afa59704ccef7127932be1d71bfdee86c0e651
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-08-15 17:26:27 +0300

    fix bugs

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-import { loginRequest } from "./actions/auth";
+// import { loginRequest } from "./actions/auth";
 import { setCookie } from "./utils";
 
 const auth = {
@@ -15,41 +15,41 @@ const auth = {
     }
 }
 
-export function useAuth() {
-    const [user, setUser] = useState(null);
-
-    const signIn = async form => {
-        const data = await loginRequest(form)
-            .then(res => {
-            let authToken = undefined;
-            console.log('res', res);
-            res.headers.forEach(header => header.startsWith('Bearer ') ? authToken = header.split(' ')[1] : header);
-            if (authToken) {
-                setCookie('token', authToken);
-            }
-            return res.json();
-        })
-            .then(data => data);;
-        console.log(data);
-
-        // return auth.signIn(() => {
-        //     console.log(form);
-        //     // Временные данные, которые будут доступны приложению
-        //     // setUser({ id: 1337, name: 'David' });
-        //     // cb();
-        // });
-    };
-
-    const signOut = cb => {
-        return auth.signOut(() => {
-            setUser(null);
-            cb();
-        });
-    };
-
-    return {
-        user,
-        signIn,
-        signOut
-    };
-}
+// export function useAuth() {
+//     const [user, setUser] = useState(null);
+//
+//     const signIn = async form => {
+//         const data = await loginRequest(form)
+//             .then(res => {
+//             let authToken = undefined;
+//             console.log('res', res);
+//             res.headers.forEach(header => header.startsWith('Bearer ') ? authToken = header.split(' ')[1] : header);
+//             if (authToken) {
+//                 setCookie('token', authToken);
+//             }
+//             return res.json();
+//         })
+//             .then(data => data);;
+//         console.log(data);
+//
+//         // return auth.signIn(() => {
+//         //     console.log(form);
+//         //     // Временные данные, которые будут доступны приложению
+//         //     // setUser({ id: 1337, name: 'David' });
+//         //     // cb();
+//         // });
+//     };
+//
+//     const signOut = cb => {
+//         return auth.signOut(() => {
+//             setUser(null);
+//             cb();
+//         });
+//     };
+//
+//     return {
+//         user,
+//         signIn,
+//         signOut
+//     };
+// }
